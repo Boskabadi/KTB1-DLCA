dbf10ec654a15a3fb5ef4fcdc14532391f3e1af6
Reza <149666756+Boskabadi@users.noreply.github.com>
2025-01-08 14:29:02 +0100

    Delete savingdata.m

--- savingdata.m
+++
@@ -1,9 +0,0 @@
-% Extract time and data from the timeseries object
-timeData = DI.Time;    % Extract the time vector
-valueData = DI.Data;   % Extract the data values
-
-% Create a table for better organization
-dataTable = table(timeData, valueData, 'VariableNames', {'Time', 'Data'});
-
-% Save the table to an Excel file
-writetable(dataTable, 'DI2.xlsx');
